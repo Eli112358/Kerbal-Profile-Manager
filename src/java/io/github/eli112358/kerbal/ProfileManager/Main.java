b3cffe1c400d953284cb75a34075a25cb27f8e98
Eli112358 <Eli112358@users.noreply.github.com>
2019-03-19 02:12:55 -0400

    Develop primary code structure

--- src/java/io/github/eli112358/kerbal/ProfileManager/Main.java
+++ src/java/io/github/eli112358/kerbal/ProfileManager/Main.java
@@ -1,6 +1,16 @@
 package io.github.eli112358.kerbal.ProfileManager;
 
+import java.io.File;
+import java.util.HashMap;
 public class Main {
+	public static HashMap<String, File> mods = new HashMap<>();
+	public static HashMap<String, Profile> profiles = new HashMap<>();
 	public static void main(String[] args) {
+	}
+	public static void reload() {
+	}
+	public static void reloadMods() {
+	}
+	public static void reloadProfiles() {
 	}
 }
--- src/java/io/github/eli112358/kerbal/ProfileManager/Profile.java
+++ src/java/io/github/eli112358/kerbal/ProfileManager/Profile.java
@@ -1,4 +1,39 @@
 package io.github.eli112358.kerbal.ProfileManager;
 
+import java.io.File;
+import java.util.ArrayList;
 public class Profile {
+	private boolean active = false;
+	private String name;
+	private File file;
+	private ArrayList<String> mods;
+	public Profile(String name) {
+		this.name=name;
+	}
+	public Profile(File file) {
+		this.file=file;
+	}
+	public String getName() {
+		return name;
+	}
+	public ArrayList<String> getMods() {
+		return mods;
+	}
+	public void addMod(String mod) {
+		mods.add(mod);
+		mods.sort(String::compareToIgnoreCase);
+	}
+	public boolean removeMod(String mod) {
+		return mods.remove(mod);
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void activate() {
+	}
+	public void reload() {
+	}
+	public File getFile() {
+		return file;
+	}
 }
