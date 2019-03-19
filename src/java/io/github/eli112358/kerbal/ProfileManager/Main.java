a38ac6bd8be04ce8cc53ff1260a2bb6387a89acf
Eli112358 <Eli112358@users.noreply.github.com>
2019-03-19 03:38:46 -0400

    Add settings and saving to Main

--- src/java/io/github/eli112358/kerbal/ProfileManager/Main.java
+++ src/java/io/github/eli112358/kerbal/ProfileManager/Main.java
@@ -3,6 +3,8 @@ package io.github.eli112358.kerbal.ProfileManager;
 import java.io.File;
 import java.util.HashMap;
 public class Main {
+	public static File settingsFile;
+	public static HashMap<String, File> settings = new HashMap<>();
 	public static HashMap<String, File> mods = new HashMap<>();
 	public static HashMap<String, Profile> profiles = new HashMap<>();
 	public static void main(String[] args) {
@@ -12,5 +14,15 @@ public class Main {
 	public static void reloadMods() {
 	}
 	public static void reloadProfiles() {
+	}
+	public static void reloadSettings() {
+	}
+	public static void save() {
+	}
+	public static void saveMods() {
+	}
+	public static void saveProfiles() {
+	}
+	public static void saveSettings() {
 	}
 }
